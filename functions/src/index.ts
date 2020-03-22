7b54e7a0cc3e8f369f52842f75ab5da6784c5e85
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2020-03-22 11:55:52 +0100

    Ban user works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,31 +5,28 @@ admin.initializeApp();
 
 //In case user is deleted from users collection
 //This method ensures that he is also deleted from auth
-exports.deleteUser = functions.firestore.document('users/{id}')
-    .onDelete((res, context) => {
-        admin.auth().deleteUser(context.params.id)
-        .then(() => console.log('delete user'))
-        .catch(err => console.error(err))
+exports.deleteUser = functions.firestore.document('users/{id}').onDelete((res, context) => {
+        return admin.auth().deleteUser(context.params.id);
     });
 
 //Block user implementation
 exports.banUser = functions.firestore.document('users/{id}')
     .onUpdate((change,context) => {
-        const id = context.params.id;
 
-        const before = change.before.data();
-        const after = change.after.data();
-
-        const hasChangedBan = (before != undefined && after != undefined && before.banned != after.banned)
-            || (after != undefined && before == undefined);
-        if(hasChangedBan){
+        return new Promise((resolve,reject) =>{
+            const id = context.params.id;
+            const prod = change.after.data() as any;
+     
             admin.auth().getUser(id)
             .then( userRecord => {
-                admin.auth().updateUser(id,{disabled: context.params.banned})
-                    .catch(() => console.log('Could not enable/disable user'));
+             if(userRecord.disabled != prod.banned)
+                admin.auth().updateUser(id,{disabled: prod.banned})
+                    .then(() => resolve('User ban updated'))
+                    .catch(err => reject(err));
+            else
+                resolve();
             })
-            .catch(()=> console.log('Couldnt retrieve user'));
-        }
-        
+            .catch(()=> reject('Couldnt retrieve user'));
+        })
     });
 

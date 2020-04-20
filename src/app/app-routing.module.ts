b9a438ac51db9cf1200477c384ea4b4413083746
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2020-04-20 09:35:58 +0200

    Removing useless stuff for Lars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,10 @@ import { AdminGuard } from './auth/shared/admin.guard';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
-  { path: 'regions', loadChildren: () => import('./regions/regions.module').then(m => m.RegionsModule) },
+  { path: '', redirectTo: 'products', pathMatch: 'full'},
   { path: 'user', loadChildren:() => import('./users/users.module').then(m=>m.UsersModule) },
   { path: 'auth', loadChildren:() => import('./auth/auth.module').then(m=>m.AuthModule)},
-  { path: 'products', loadChildren:() => import('./products/products.module').then(m=>m.ProductsModule)},
-  { path: 'admin', loadChildren:() => import('./admin/admin.module').then(m=>m.AdminModule), canLoad: [AdminGuard]}
+  { path: 'products', loadChildren:() => import('./products/products.module').then(m=>m.ProductsModule)}
 ];
 
 @NgModule({
--- src/app/regions/shared/region.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Region {
-    uid: string;
-    name: string;
-    hunterCount?: number;
-    animalCount?: number;
-    maxHunters: number;
-}
